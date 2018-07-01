78a0f61b5a4062fa5e81fd9b94871d8ebee478c4
Chuck Cho <ccho@axon.com>
2018-06-30 19:30:35 -0700

    clean up

--- exploratory/print_labels.py
+++ exploratory/print_labels.py
@@ -1,46 +1,31 @@
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import tensorflow as tf
+import glob
 import os
-
-data_path = '/media/6TB/video/yt8m-v2'
-video_data_path = os.path.join(data_path, 'video')
+import pandas as pd
+import tensorflow as tf
 
 labels_df = pd.read_csv(os.path.join('..', 'label_names_2018_fixed.csv'))
 print(labels_df.head())
 print("Total nubers of labels in sample dataset: %s" %(len(labels_df['label_name'].unique())))
 
-video_files = [os.path.join(video_data_path, vf) for vf in os.listdir(video_data_path)]
+video_data_path = '/media/6TB/video/yt8m-v2/video'
+video_files = glob.glob(os.path.join(video_data_path, "train????.tfrecord"))
 
 # let's just use the first sample
 video_files = [video_files[0]]
 print("Will take a look at {}...".format(video_files))
 
-vid_ids = []
-labels = []
-
-for file in video_files:
-    for example in tf.python_io.tf_record_iterator(file):
+for video_file in video_files:
+    for example in tf.python_io.tf_record_iterator(video_file):
         tf_example = tf.train.Example.FromString(example)
+        vid_id = tf_example.features.feature['id'].bytes_list.value[0].decode(encoding='UTF-8')
+        labels = tf_example.features.feature['labels'].int64_list.value
 
-        vid_ids.append(tf_example.features.feature['id'].bytes_list.value[0].decode(encoding='UTF-8'))
-        labels.append(tf_example.features.feature['labels'].int64_list.value)
-
-print('Number of videos in Sample data set: %s' % str(len(vid_ids)))
-
-for video_index in range(len(vid_ids)):
-
-    vid_id = vid_ids[video_index]
-
-    # Lets convert labels for each video into their respective names
-    labels_name = []
-    for row in labels:
-        n_labels = []
-        for label_id in row:
+        # Lets convert labels for each video into their respective names
+        label_names = []
+        for label_id in labels:
             # some labels ids are missing so have put try/except
             try:
-                n_labels.append(str(labels_df[labels_df['label_id']==label_id]['label_name'].values[0]))
+                label_names.append(str(labels_df[labels_df['label_id']==label_id]['label_name'].values[0]))
             except:
                 continue
-        labels_name.append(n_labels)
-
-    print('Check video_id at https://data.yt8m.org/2/j/i/{}/{}.js : labels={}'.format(vid_id[:2], vid_id, str(labels_name[video_index])))
+        print('Check video_id at https://data.yt8m.org/2/j/i/{}/{}.js : labels={}'.format(vid_id[:2], vid_id, str(label_names)))
