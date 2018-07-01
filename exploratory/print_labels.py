1b499eae8350770908a2da5f27b8a65b7ddbb916
Chuck Cho <ccho@axon.com>
2018-06-30 22:46:11 -0700

    Option to save IDs or descriptions

--- exploratory/print_labels.py
+++ exploratory/print_labels.py
@@ -3,16 +3,18 @@ import os
 import pandas as pd
 import tensorflow as tf
 
-labels_df = pd.read_csv(os.path.join('..', 'label_names_2018_fixed.csv'))
-print(labels_df.head())
-print("Total nubers of labels in sample dataset: %s" %(len(labels_df['label_name'].unique())))
+# whether print label names or label ids (integer)
+print_label_names = False
 
 video_data_path = '/media/6TB/video/yt8m-v2/video'
-video_files = glob.glob(os.path.join(video_data_path, "train????.tfrecord"))
+#video_files = glob.glob(os.path.join(video_data_path, "validate???5.tfrecord"))
+video_files = glob.glob(os.path.join(video_data_path, "train???[13579].tfrecord"))
 
-# let's just use the first sample
-video_files = [video_files[0]]
-print("Will take a look at {}...".format(video_files))
+if print_label_names:
+    labels_df = pd.read_csv(os.path.join('..', 'label_names_2018_fixed.csv'))
+    #print(labels_df.head())
+    #with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
+    #    print(labels_df)
 
 for video_file in video_files:
     for example in tf.python_io.tf_record_iterator(video_file):
@@ -21,11 +23,16 @@ for video_file in video_files:
         labels = tf_example.features.feature['labels'].int64_list.value
 
         # Lets convert labels for each video into their respective names
-        label_names = []
-        for label_id in labels:
-            # some labels ids are missing so have put try/except
-            try:
-                label_names.append(str(labels_df[labels_df['label_id']==label_id]['label_name'].values[0]))
-            except:
-                continue
-        print('Check video_id at https://data.yt8m.org/2/j/i/{}/{}.js : labels={}'.format(vid_id[:2], vid_id, str(label_names)))
+        if print_label_names:
+            label_names = []
+            for label_id in labels:
+                # some labels ids are missing so have put try/except
+                try:
+                    label_names.append(str(labels_df[labels_df['label_id']==label_id]['label_name'].values[0]))
+                except:
+                    continue
+            #print('Check video_id at https://data.yt8m.org/2/j/i/{}/{}.js : labels={}'.format(vid_id[:2], vid_id, str(label_names)))
+            #print('{}: {}'.format(vid_id, labels))
+            print('{}: {}'.format(vid_id, str(label_names)))
+        else:
+            print('{}: {}'.format(vid_id, ' '.join(str(x) for x in labels)))
