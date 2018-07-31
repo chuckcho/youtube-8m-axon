37a0618a674e1e5d76c23f8a5372c263b3252676
Chuck Cho <ccho@axon.com>
2018-07-31 02:51:58 -0700

    Changed file names around

--- ensemble/file_averaging.py
+++ ensemble/file_averaging.py
@@ -1,66 +1,68 @@
-# Copyright 2017 Antoine Miech All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS-IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-'''
-This is an obsolete script from https://github.com/antoine77340/Youtube-8M-WILLOW/blob/master/file_averaging.py
-For large inference files or for many files, this script is likely to fail because it loads all the inferences at the same time.
-'''
-
-import os
 from collections import defaultdict, Counter
-import pickle
-import pandas as pd
+import glob
+from itertools import izip
+import os
+import subprocess
+import sys
+import time
 
-SUBMIT_PATH = '/media/TB2/chuck/__MODEL_VAULT__'
-SIGFIGS = 6
+inference_dir = '/media/TB2/chuck/__MODEL_VAULT__'
+#inference_dir = '/tmp/__MODEL_VAULT__'
+significant_digits = 6
+top_k = 50
+show_progress_iter = 20000
 
-def read_models(model_weights, blend=None):
-    if not blend:
-        blend = defaultdict(Counter)
-    for m, w in model_weights.items():
-        print(m, w)
-        with open(os.path.join(SUBMIT_PATH, m + '.csv'), 'r') as f:
-            f.readline()
-            for l in f:
-                id, r = l.split(',')
-                r = r.split(' ')
-                n = len(r) // 2
-                for i in range(0, n * 2, 2):
-                    k = int(r[i])
-                    v = int(10**(SIGFIGS - 1) * float(r[i+1]))
-                    blend[id][k] += w * v
-    return blend
+submission_ready = False
+if submission_ready:
+  top_k = 20
 
+def file_len(fname):
+  p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE)
+  result, err = p.communicate()
+  if p.returncode != 0:
+      raise IOError(err)
+  return int(result.strip().split()[0])
 
-def write_models(blend, file_name, total_weight):
-    with open(os.path.join(SUBMIT_PATH, file_name + '.csv'), 'w') as f:
-        f.write('VideoID,LabelConfidencePairs\n')
-        for id, v in blend.items():
-            l = ' '.join(['{} {:{}f}'.format(t[0]
-                                            , float(t[1]) / 10 ** (SIGFIGS - 1) / total_weight
-                                            , SIGFIGS) for t in v.most_common(20)])
-            f.write(','.join([id, l + '\n']))
-    return None
+def do_stuff(model_pred, output_file, total_weight=None):
+  total_num_vids = file_len(model_pred[0])
+  if total_weight is None:
+    total_weight = len(model_pred)
+  files = [open(filename) for filename in model_pred]
+  out_fp = open(output_file, 'w')
 
+  # add header
+  if submission_ready:
+    out_fp.write('VideoId,LabelConfidencePairs\n')
 
-model_pred = {
-        'inference-on-train-and-val0-4+6-9-model03-02-cp473348-top50': 1,
-        'inference-on-train-and-val0-4+6-9-model05-02-cp800020-top50': 1,
-        'inference-on-train-and-val0-4+6-9-model06-02-cp173998-top50': 1,
-        'inference-on-train-and-val0-4+6-9-model07-01-cp380325-top50': 1,
-        'inference-on-train-and-val0-4+6-9-model07-02-cp243673-top50': 1,
-        }
+  start_time = time.time()
+  for line_count, lines in enumerate(izip(*files)):
+    blend = Counter()
+    if line_count % show_progress_iter == 0:
+      elapsed = (time.time() - start_time)
+      throughput = float(line_count+1) / elapsed
+      eta = elapsed * total_num_vids / (line_count+1) / 60.0
+      print("[Info] Progress={:2.1f}% ({}/{}), Throughput={:.1f} lines/s, ETA={:.1f} min".format(
+              float(line_count+1) / total_num_vids * 100,
+              line_count+1,
+              total_num_vids,
+              throughput,
+              eta,
+              ))
 
-avg = read_models(model_pred)
-write_models(avg, 'inference-ens5-0302-0502-0602-0701-0702', sum(model_pred.values()))
+    for l in lines:
+      id, r = l.split(',')
+      r = r.split(' ')
+      assert len(r) % 2 == 0, "Number of fields is odd. That's odd!"
+      for i in range(0, len(r), 2):
+        k = int(r[i])
+        v = int(10**(significant_digits - 1) * float(r[i+1]))
+        blend[k] += v
+    l = ' '.join(['{} {:{}f}'.format(t[0]
+            , float(t[1]) / 10 ** (significant_digits - 1) / total_weight
+            , significant_digits) for t in blend.most_common(top_k)])
+    out_fp.write(','.join([id, l + '\n']))
+
+model_pred = glob.glob(os.path.join(inference_dir, "*-sorted-noheader.csv"))
+print "[Info] model_pred={}".format(model_pred)
+do_stuff(model_pred, 'inference-ens{}.csv'.format(len(model_pred)))
